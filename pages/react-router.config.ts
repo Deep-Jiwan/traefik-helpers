3463ec0fa66364d5b5aec0712f73f26451c9de8c
Deep Jiwan <deepjiwan30@gmail.com>
2025-12-10 16:20:07 +0530

    use react build

--- pages/react-router.config.ts
+++ pages/react-router.config.ts
@@ -5,4 +5,12 @@ export default {
   ssr: false,
   // Configure build to inline assets
   buildDirectory: "build",
+  // Prerender static pages for each error code
+  prerender: async () => {
+    const errorCodes = ['401', '403', '404', '405', '500', '502', '503'];
+    return [
+      "/",
+      ...errorCodes.map(code => `/${code}`),
+    ];
+  },
 } satisfies Config;
